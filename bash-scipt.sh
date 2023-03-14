1008298a28008e83f8d0fb9e9cca86f72f180f15
congtusoma146 <quangduy06.96@gmail.com>
2023-03-14 15:45:14 +0700

    sudo chown

--- bash-scipt.sh
+++ bash-scipt.sh
@@ -1,6 +1,6 @@
 whoami
 ls -la
-chown jenkins:root /var/lib/jenkins/workspace/bash-scipt.sh/
+sudo chown jenkins:root /var/lib/jenkins/workspace/bash-scipt.sh/
 chmod -R a+rwx  /var/lib/jenkins/workspace/bash-scipt.sh/
 
 ls -la
