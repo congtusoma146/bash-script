c9e5fb735761aed0164134209059c2995e4030de
congtusoma146 <quangduy06.96@gmail.com>
2023-03-14 15:47:21 +0700

    admin:jenkins

--- bash-scipt.sh
+++ bash-scipt.sh
@@ -1,6 +1,6 @@
 whoami
 ls -la
-sudo chown jenkins:admin /var/lib/jenkins/workspace/bash-scipt.sh
+sudo chown admin:jenkins /var/lib/jenkins/workspace/bash-scipt.sh
 chmod -R a+rwx  /var/lib/jenkins/workspace/bash-scipt.sh
 
 ls -la
