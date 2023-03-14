a04af9d7ec59518d05fa44bff235223590d8b7ef
congtusoma146 <quangduy06.96@gmail.com>
2023-03-14 15:37:52 +0700

    /

--- bash-scipt.sh
+++ bash-scipt.sh
@@ -1,6 +1,5 @@
 whoami
 ls -la
-chmod -R a+x+w  /var/lib/jenkins/workspace/bash-script
-chown root:jenkins /var/lib/jenkins/workspace/bash-script
+chmod -R a+x+w  /var/lib/jenkins/workspace/bash-script/
 ls -la
 mkdir /hello-world1/
