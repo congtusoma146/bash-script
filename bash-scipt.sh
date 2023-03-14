93fda81796a5563cd49e9f5736304cf185fca9ac
congtusoma146 <quangduy06.96@gmail.com>
2023-03-14 15:44:21 +0700

    delete sudo

--- bash-scipt.sh
+++ bash-scipt.sh
@@ -1,7 +1,7 @@
 whoami
 ls -la
-sudo chown jenkins:root /var/lib/jenkins/workspace/bash-scipt.sh/
-sudo chmod -R a+rwx  /var/lib/jenkins/workspace/bash-scipt.sh/
+chown jenkins:root /var/lib/jenkins/workspace/bash-scipt.sh/
+chmod -R a+rwx  /var/lib/jenkins/workspace/bash-scipt.sh/
 
 ls -la
 mkdir /hello-world1/
